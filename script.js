0473ab89841dc3d289c0784ef8bfa2684c198f3f
Peter <popazovpeta@gmail.com>
2024-03-09 23:45:02 +0200

    bugs fixed

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ bntNav.addEventListener("click", function () {
 const currentYearEl = document.querySelector(".year");
 const year = new Date().getFullYear();
 currentYearEl.innerHTML = year;
+
+const sectionCards = document.querySelector(".obeserver-target");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    console.log(entries);
+  },
+  {
+    root: null,
+    threshold: 1,
+  }
+);
+obs.observe(sectionCards);
